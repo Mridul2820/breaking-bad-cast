5c05a6774c3d0e411bf859d68b157048323a13bf
Mridul2820 <immridulpanda2820@gmail.com>
2021-01-28 14:31:45 +0530

    characters added to the ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from 'react';
+import './App.scss';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/UI/Header";
+import CharactersGrid from "./components/characters/CharactersGrid";
+
+const App = () => {
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+            console.log(result.data)
+            setItems(result.data)
+            setIsLoading(false)
+        }
+
+        fetchItems()
+    }, [])
+
+    return (
+        <div className="container">
+            <Header />
+            <CharactersGrid isLoading={isLoading} items={items} />
+        </div>
+    );
 }
 
 export default App;
